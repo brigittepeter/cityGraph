a3520b447496273f923a620b52c15e69b5af239d
brigittepeter <brigittepeter@users.noreply.github.com>
2018-04-04 09:53:07 +0200

    Initialisierung der Städte

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,8 +1,46 @@
 package main;
 
+import stadt.Stadt;
+
 public class Main {
 
     public static void main (String args[]){
+
+        Stadt frankfurt = new Stadt();
+        frankfurt.setNameStadt("Frankfurt");
+        frankfurt.setEinwohnerZahl(736414);
+        frankfurt.setFlaeche(248.31);
+
+        Stadt nuernberg = new Stadt();
+        nuernberg.setNameStadt("Nuernberg");
+        nuernberg.setEinwohnerZahl(526920);
+        nuernberg.setFlaeche(186.5);
+
+        Stadt muenchen = new Stadt();
+        muenchen.setNameStadt("Muenchen");
+        muenchen.setEinwohnerZahl(1545105);
+        muenchen.setFlaeche(310.71);
+
+        Stadt augsburg = new Stadt();
+        augsburg.setNameStadt("Augsburg");
+        augsburg.setEinwohnerZahl(290743);
+        augsburg.setFlaeche(146.8);
+
+        Stadt ulm = new Stadt();
+        ulm.setNameStadt("Ulm");
+        ulm.setEinwohnerZahl(124781);
+        ulm.setFlaeche(118.7);
+
+        Stadt stuttgart = new Stadt();
+        stuttgart.setNameStadt("Stuttgart");
+        stuttgart.setEinwohnerZahl(611213);
+        stuttgart.setFlaeche(207.4);
+
+        Stadt freiburg = new Stadt();
+        freiburg.setNameStadt("Freiburg");
+        freiburg.setEinwohnerZahl(227400);
+        freiburg.setFlaeche(153.1);
+        
 
     }
 }
