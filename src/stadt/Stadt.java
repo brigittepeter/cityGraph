35e802af153035d2558f2dd66c994f7d510a3cff
brigittepeter <brigittepeter@users.noreply.github.com>
2018-04-04 08:54:16 +0200

    test

--- src/stadt/Stadt.java
+++ src/stadt/Stadt.java
@@ -1,4 +1,43 @@
 package stadt;
 
 public class Stadt {
+    String nameStadt;
+    int einwohnerZahl;
+    double flaeche;
+    List <Verbindung> verbindungen;
+
+    public String getNameStadt() {
+        return nameStadt;
+    }
+
+    public void setNameStadt(String nameStadt) {
+        this.nameStadt = nameStadt;
+    }
+
+    public int getEinwohnerZahl() {
+        return einwohnerZahl;
+    }
+
+    public void setEinwohnerZahl(int einwohnerZahl) {
+        this.einwohnerZahl = einwohnerZahl;
+    }
+
+    public double getFlaeche() {
+        return flaeche;
+    }
+
+    public void setFlaeche(double flaeche) {
+        this.flaeche = flaeche;
+    }
+
+    public List<Verbindung> getVerbindungen() {
+        return verbindungen;
+    }
+
+    public void setVerbindungen(List<Verbindung> verbindungen) {
+        this.verbindungen = verbindungen;
+    }
 }
+
+
+
